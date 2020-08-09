725be5a8ff01cff9ab77e81e7e4cf97806575da5
Ezekiel <ezekielphlat@gmail.com>
2020-08-09 23:36:22 +0100

    Hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main()  => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My First App'),
-      centerTitle: true,
-      backgroundColor: Colors.green[600],
-    ),
-    body: Center(
-      child: Text(
-        'Hello Ephlat',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.blueGrey,
-          fontFamily: 'lemonada',
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hello Ephlat',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.blueGrey,
+            fontFamily: 'lemonada',
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('Click'),
-      backgroundColor: Colors.green,
-    ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.green,
+      ),
 
-  ),
-));
+    );
+  }
+}
 
 
