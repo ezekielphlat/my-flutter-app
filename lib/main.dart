5aae7646c0ca2e67426b67c6239c1223ea7a48ff
Ezekiel <ezekielphlat@gmail.com>
2020-08-09 06:58:33 +0100

    Add Colors and Font Family

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,26 @@ void main()  => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('My First App'),
       centerTitle: true,
+      backgroundColor: Colors.green[600],
     ),
     body: Center(
-      child: Text('Hello Ephlat'),
+      child: Text(
+        'Hello Ephlat',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.blueGrey,
+          fontFamily: 'lemonada',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: () {},
       child: Text('Click'),
+      backgroundColor: Colors.green,
     ),
-    
+
   ),
 ));
 
